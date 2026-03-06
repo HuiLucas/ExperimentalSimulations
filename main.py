f78d1c21c6cce153a01eaa1d6805987c9cb43a0a
marnick1268 <M.A.Stoll@student.tudelft.nl>
2026-03-06 16:32:36 +0100

    main

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ if __name__=='__main__':
     data_modeloff = loaded_data('data/modeloff.mat', 'modeloff')
 
     elev_approx_10 = data_normal_configuration.filter(dE__ge=9.5, dE__le=10.5, test_point_id__ne='46', wind_condition='windOn')  # Use 'windOff' for wind-off data
+    propoff_basi = data_propoff.filter(dE__ge=9.5, dE__le=10.5,V__ge=29.5,V__le=30.5,dR__ge=-0.5, dR__le=0.5,AoS__ge=-0.0005, AoS__le=0.0005,wind_condition='windOn')  # Use 'windOff' for wind-off data
+    print(propoff_basi['AoS'].values)
     bc=BoundaryCorrections(
-            CD_0=0.07,
+            CD_0=0.07,  #from prop off data but needs to be refined to make exact TODO lckz huirne realllllllltytytyyy wants to iterate since he is bored
             V_unc=elev_approx_10['V'].values,
             rho=elev_approx_10['rho'].values,
             q_unc=elev_approx_10['q'].values,
@@ -20,7 +22,8 @@ if __name__=='__main__':
             CL_unc=elev_approx_10['CL'].values,
             CD_unc=elev_approx_10['CD'].values,
             CM_c4_unc=elev_approx_10['CMpitch25c'].values,
-            CL_alpha=0.11106)
+            CL_alpha=0.11106   #from prop off data but needs to be refined to make exact
+            )
     
     alpha_cor, V_cor, q_cor, CL_cor, CD_cor, CM_c4_cor=bc.apply_boundary_corrections()
     aoa = elev_approx_10['AoA'].values
@@ -30,13 +33,25 @@ if __name__=='__main__':
     CD = elev_approx_10['CD'].values
     CM_c4 = elev_approx_10['CMpitch25c'].values
     fig=plt.figure()
-    ax=fig.subplots(1,3)
-    ax[0].plot(aoa,CL,label='uncorrected')
-    ax[0].plot(alpha_cor,CL_cor='corrected')
-    plt.legend()
+    ax=fig.subplots(1,4)
+    ax[0].scatter(aoa,CL,label='uncorrected')
+    ax[0].scatter(alpha_cor,CL_cor,label='corrected')
+    ax[0].legend()
+
+    ax[1].scatter(aoa,CD,label='uncorrected')
+    ax[1].scatter(alpha_cor,CD_cor,label='corrected')
+    ax[1].legend()
+
+    ax[2].scatter(CL,CD,label='uncorrected')
+    ax[2].scatter(CL_cor,CD_cor,label='corrected')
+    ax[2].legend()
+
+    ax[3].scatter(propoff_basi['AoA'].values,propoff_basi['CL'].values,label='uncorrected')
+#     a3[2].scatter(CL_cor,CD_cor,label='corrected')
+    ax[3].legend()
 
     for axs in ax:
-        ax.grid()
+        axs.grid()
     plt.show()
 
 
